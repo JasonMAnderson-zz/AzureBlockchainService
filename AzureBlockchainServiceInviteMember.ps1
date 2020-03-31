c362fe47cd5c6863b9fac814dfecca1473e1da5c
Jason Anderson <jason.m.anderson@microsoft.com>
2020-03-31 09:16:03 -0700

    Update AzureBlockchainServiceInviteMember.ps1

--- AzureBlockchainServiceInviteMember.ps1
+++ AzureBlockchainServiceInviteMember.ps1
@@ -1,3 +1,6 @@
+#  This script provides a quick way to invite a member, providing feedback after invitiation of the invite status.
+#  Note: You must run the AzureBlockchainServiceConnection.ps1 script prior to running this script, as it establishes the variables below.
+
 $ContractConnection | New-BlockchainMemberInvitation -SubscriptionId <AzureSubscriptionID> -Role USER -Web3Account $MemberAccount
 $ContractConnection | Get-BlockchainMember
 $ContractConnection | Get-BlockchainMemberInvitation
